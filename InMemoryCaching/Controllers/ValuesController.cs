3a7c0f7557a4c9057d172f36c23c45cb6d4073db
Hakan <ahmethakanozturk34@gmail.com>
2023-09-29 15:36:01 +0300

    ValuesController Added
    -Add Set and Get methods

---
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("[action]/{name}")]
+        public void Set(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]")]
+        public string Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
