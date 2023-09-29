a42b0bae60bb9499dfb0f702849613aae19f7623
Hakan <ahmethakanozturk34@gmail.com>
2023-09-29 17:45:26 +0300

    Distributed Caching Completed
    -Added expiration times.
    -Added control for data exist in cache or not.

---
+++ DistributedCaching/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace DistributedCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            await _distributedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(20),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(20),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distributedCache.GetStringAsync("name");
+            var surnameRaw = await _distributedCache.GetAsync("surname");
+            string surname;
+
+            if (name is not null && surnameRaw is not null)
+                surname = Encoding.UTF8.GetString(surnameRaw);
+            else
+                throw new Exception("Value Could't Find at Cach!");
+            return Ok(new
+            {
+                name,
+                surname
+            });
+        }
+    }
+}
