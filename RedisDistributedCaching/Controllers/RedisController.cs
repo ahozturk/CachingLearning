0c15901e9b870d592225dd7bdf00fbd798dd5306
Hakan <ahmethakanozturk34@gmail.com>
2023-10-13 15:00:53 +0300

    Distributed Caching Example

---
+++ RedisDistributedCaching/Controllers/RedisController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisDistributedCaching.Services;
+
+namespace RedisDistributedCaching.Controllers
+{
+    public class RedisController : Controller
+    {
+        [HttpGet("[action]/{key}/{value}")]
+        public async Task<IActionResult> SetValue(string key, string value)
+        {
+            var redisMaster = await RedisService.GetRedisMasterDatabase();
+            await redisMaster.StringSetAsync(key, value);
+            return Ok();
+        }
+
+        [HttpGet("[action]/{key}")]
+        public async Task<IActionResult> GetValue(string key)
+        {
+            var redisMaster = await RedisService.GetRedisMasterDatabase();
+            var responseValue = await redisMaster.StringGetAsync(key);
+            return Ok(responseValue.ToString());
+        }
+    }
+}
